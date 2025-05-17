4b425d337c02313fa1463b175b5af830d1986512
azizbekxamidxonov <assalomualeykum2007@gmail.com>
2025-05-17 16:04:50 +0500

    finished lessons

---
+++ Lessons/7-dars/index.js
@@ -0,0 +1,19 @@
+
+function salom() {
+  let ism = prompt("Ismingizni kiriting!!!");
+  console.log("Salom", ism );
+}
+
+salom();
+
+
+let number1 = 10
+
+let number2 = 20
+
+function calcAction(a,b){
+    console.log(a+b);
+    
+}
+
+calcAction(number1, number2)
