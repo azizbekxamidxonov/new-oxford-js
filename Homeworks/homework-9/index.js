fcfd7202cec0fbf609d562ebec5ee21dc6d430aa
azizbekxamidxonov <assalomualeykum2007@gmail.com>
2025-05-27 06:59:08 +0500

    finished homewrok

---
+++ Homeworks/homework-9/index.js
@@ -0,0 +1,20 @@
+let student = {
+    firstName: "Eshmat",
+    lastName: "Toshmatov",
+    age: 20,
+    course: "Frontend"
+};
+
+console.log(student.firstName + " " + student.lastName + " " + student.age);
+
+let data = {
+    name: "Phone",
+    model: "iPhone",
+    price: 700,
+    quantity: 8
+};
+
+let totalPrice = price * quantity;
+
+console.log("Umumiy narxi:", totalPrice);
+
