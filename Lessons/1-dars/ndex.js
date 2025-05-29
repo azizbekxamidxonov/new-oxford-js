1decef90354f5c64a3edb6aca05025a6560bdaeb
azizbekxamidxonov <assalomualeykum2007@gmail.com>
2025-05-29 15:43:36 +0500

    finished homework

--- Lessons/1-dars/ndex.js
+++
@@ -1,5 +0,0 @@
-let a = 12
-let b = 19
-let c = 9
-let e = 5
-console.log(a + (b - c)*e)
---
+++ Lessons/lesson-1/index.js
@@ -0,0 +1,6 @@
+let yil = prompt("Tug‘ilgan yilingizni kiriting:");
+let hozirgiYil = 2025;
+let yosh = hozirgiYil * yil;
+
+console.log(" " + yosh + " yoshdasiz");
+
