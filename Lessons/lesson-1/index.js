c3c4987925516a0d70806bd00633fb646259cd85
azizbekxamidxonov <assalomualeykum2007@gmail.com>
2025-05-31 14:10:47 +0500

    changed

--- Lessons/lesson-1/index.js
+++ Lessons/lesson-1/index.js
@@ -1,6 +1,11 @@
-let yil = prompt("Tug‘ilgan yilingizni kiriting:");
-let hozirgiYil = 2025;
-let yosh = hozirgiYil * yil;
+// let yil = prompt("Tug‘ilgan yilingizni kiriting:");
+// let hozirgiYil = 2025;
+// let yosh = hozirgiYil * yil;
 
 console.log(" " + yosh + " yoshdasiz");
+let yil = prompt("Yilni kiriting (masalan: 1500):");
+let boshlanish = new Date(yil + "-01-01");
+let bugun = new Date();
+let kunlar = (bugun - boshlanish) / (1000 * 60 * 60 * 24);
+alert("Kiritilgan yildan hozirgacha kunlar soni: " +kunlar);
 
