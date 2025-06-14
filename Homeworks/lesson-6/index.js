06ceb70b10befd7aa284a77698c4dba2d834efe2
azizbekxamidxonov <assalomualeykum2007@gmail.com>
2025-06-14 13:41:45 +0500

    finished

---
+++ Homeworks/lesson-6/index.js
@@ -0,0 +1,51 @@
+// let son;
+
+// do {
+//   son = prompt("Iltimos, 100 dan katta son kiriting:");
+// } while (son !== null && son.trim() !== "" && Number(son) <= 100);
+
+
+// ==============================
+
+// let i = 0;
+
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 5);
+
+
+// ==========================
+
+// let i = -1;
+
+// do {
+//   ++i;
+//   console.log(i);
+// } while (i < 4);
+
+
+// ===============================
+
+
+
+// let n = Number(prompt("n ni kiriting (2 dan katta):"));
+// let i = 2;
+
+// do {
+//   let tub = true;
+//   let j = 2;
+
+//   do {
+//     if (i % j === 0 && j !== i) {
+//       tub = false;
+//     }
+//     j++;
+//   } while (j < i);
+
+//   if (tub) {
+//     console.log(i);
+//   }
+
+//   i++;
+// } while (i <= n);
