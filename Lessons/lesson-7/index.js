270aa8424bfeb3de54070e2cc78d5d1a1c57399c
azizbekxamidxonov <assalomualeykum2007@gmail.com>
2025-06-17 13:30:40 +0500

    finished

--- Lessons/lesson-7/index.js
+++ Lessons/lesson-7/index.js
@@ -10,16 +10,16 @@
 //   topishArifmetik(a, b, c);
   
 
-function topishGeometrik(a, b, c) {
-    let kopaytma = a * b * c;
-    let natija = kopaytma ** (1 / 3); // 3-darajali ildiz
-    console.log("O‘rtacha geometrik qiymat:", natija);
-  }
+// function topishGeometrik(a, b, c) {
+//     let kopaytma = a * b * c;
+//     let natija = kopaytma ** (1 / 3); // 3-darajali ildiz
+//     console.log("O‘rtacha geometrik qiymat:", natija);
+//   }
   
-  let a = Number(prompt("1-sonni kiriting:"));
-  let b = Number(prompt("2-sonni kiriting:"));
-  let c = Number(prompt("3-sonni kiriting:"));
+//   let a = Number(prompt("1-sonni kiriting:"));
+//   let b = Number(prompt("2-sonni kiriting:"));
+//   let c = Number(prompt("3-sonni kiriting:"));
   
-  topishGeometrik(a, b, c);
+//   topishGeometrik(a, b, c);
   
   
